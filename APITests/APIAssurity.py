060fcf2a9050c4c88fe30c25a265c8935bb2a59d
sumedh.tambe <Sumedh.Tambe@effective.digital>
2019-04-02 21:30:14 +0530

    Added logger class. Added external libs (imported libs from pip install requests package). Added project settings files. Wrote two test cases for first two Acceptance criteria.

---
+++ APITests/APIAssurity.py
@@ -0,0 +1,49 @@
+'''
+Created on 01-Apr-2019
+
+@author: Sumedh.Tambe
+'''
+import unittest
+import Lib.Logger
+import json
+import requests
+from requests.models import Response
+
+
+class APIAssurity(unittest.TestCase):
+    def setUp(self):
+        self.ApiUrl='https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false' 
+        self.Log_Level='info'       
+        self.Log=Lib.Logger.Logger(self.Log_Level)
+        self.StatusCode=requests.get(self.ApiUrl).status_code
+        
+        try:
+            if(self.StatusCode!='200'):
+                self.Response=requests.get(self.ApiUrl)
+                self.JsonData=self.Response.json()
+                
+        except Response.raise_for_status():
+                self.Log.Info("Issues in cousnuming API")
+                self.Log.Error(self.StatusCode)
+        
+    def test_Name(self):        
+        self.Log.Info("Checking name from json response")
+        self.AcceptanceCriteria='Carbon credits'
+        self.assertEqual(self.JsonData['Name'], self.AcceptanceCriteria)
+    
+    def test_IsCanReListTrue(self):
+        self.Log.Info("Checking if CanReList returns true in json response")
+        self.AcceptanceCriteria='true'
+        self.assertEqual(self.JsonData['CanRelist'], self.AcceptanceCriteria)
+    
+            
+            
+        
+        
+        
+
+
+
+
+
+
---
+++ Lib/Logger.py
@@ -0,0 +1,41 @@
+'''
+Created on 01-Apr-2019
+
+@author: Sumedh.Tambe
+'''
+import  time
+import os
+ 
+class Logger():
+    
+    def __init__(self,LOG_LEVEL):
+        
+        self.LOG_LEVEL = LOG_LEVEL
+        self.dict = {'DEBUG': 2, 'INFO': 1, 'ERROR': 0}     
+        
+        self.PWorkingDirectory = os.getcwd()
+             
+    def Debug(self,msg):
+        targetfile=open(self.PWorkingDirectory+'Log\Log.log','a')
+        if(self.dict(self.LOG_LEVEL.upper()>=2)):
+            print(time.strftime("%d/%m/%Y %I:%M:%S %p")+"DEBUG"+str(msg))
+            targetfile.write(time.strftime("%d/%m/%Y %I:%M:%S %p")+"DEBUG:"+str(msg))
+            targetfile.write("\n")
+            targetfile.close()
+    def Info(self, msg):
+        target = open(self.PWorkingDirectory + 'Log\log1.log', 'a')
+        if self.dict[self.LOG_LEVEL.upper()] >= 1:
+            print (time.strftime("%d/%m/%Y %I:%M:%S %p") + " INFO: " + (str(msg)))
+            target.write(time.strftime("%d/%m/%Y %I:%M:%S %p") + " Info: " + (str(msg)))
+            target.write('\n')
+            target.close()                 
+ 
+ 
+    def Error(self, msg):
+        target = open(self.PWorkingDirectory + 'Log\log1.log', 'a')
+        if self.dict[self.LOG_LEVEL.upper()] > 0:
+            print (time.strftime("%d/%m/%Y %I:%M:%S %p") + " ERROR: " + (str(msg)))
+            target.write(time.strftime("%d/%m/%Y %I:%M:%S %p") + " Error: " + (str(msg)))
+            target.write('\n')
+            target.close()
+        
