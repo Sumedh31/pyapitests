b636485649a76f1739a439b7b49bae7e3d3eb918
sumedh.tambe <Sumedh.Tambe@effective.digital>
2019-04-03 22:09:16 +0530

    Tests will now run in order required from the acceptance criteria given. Updated logs to be created dynamically, previously they were getting created where the script is run causing tests to fail when run from any other location.

--- APITests/APIAssurity.py
+++ APITests/APIAssurity.py
@@ -4,8 +4,7 @@ Created on 01-Apr-2019
 @author: Sumedh.Tambe
 '''
 import unittest
-import Lib.Logger
-import json
+from Lib.Logger import Logger
 import requests
 from requests.models import Response
 
@@ -13,7 +12,7 @@ class APIAssurity(unittest.TestCase):
     def setUp(self):
         self.ApiUrl='https://api.tmsandbox.co.nz/v1/Categories/6327/Details.json?catalogue=false' 
         self.Log_Level='info'       
-        self.Log=Lib.Logger.Logger(self.Log_Level)
+        self.Log=Logger(self.Log_Level)
         self.StatusCode=requests.get(self.ApiUrl).status_code
         
         try:
@@ -25,23 +24,23 @@ class APIAssurity(unittest.TestCase):
                 self.Log.Info("Issues in cousnuming API")
                 self.Log.Error(self.StatusCode)
      
-    def test_Name(self):        
+    def test_FirstAcceptanceCriteriaForName(self):        
         self.Log.Info("Checking name from json response")
         self.AcceptanceCriteria='Carbon credits'
         self.assertEqual(self.JsonData['Name'], self.AcceptanceCriteria)
      
      
-    def test_IsCanReListTrue(self):
+    def test_SecondAcceptanceCriteriaForIsCanReListTrue(self):
         self.Log.Info("Checking if CanReList returns true in json response")
         self.AcceptanceCriteria=True        
         self.assertEqual(self.JsonData['CanRelist'], self.AcceptanceCriteria)
     
-    def test_CheckDescriptionContent(self):
+    def test_ThirdAcceptanceCriteriaForCheckDescriptionContent(self):
         self.Log.Info("Checking the description in Promotions element that has Name as Gallery and description as 2x larger image")
         self.AcceptanceCriteria='2x larger image'
         self.PassedTest=False
         if (self.AcceptanceCriteria in self.JsonData['Promotions'][1]['Description']):
-            self.Log.Info("Acceptance criteria '%s' is in the json response '%s'"%(self.AcceptanceCriteria,self.JsonData['Promotions'][1]['Description']))
+            self.Log.Info("Acceptance criteria '%s' is Present in the json response '%s'"%(self.AcceptanceCriteria,self.JsonData['Promotions'][1]['Description']))
             self.PassedTest=True
         self.assertTrue(self.PassedTest,'Test Case Failed')
         
--- Lib/Logger.py
+++ Lib/Logger.py
@@ -13,17 +13,17 @@ class Logger():
         self.LOG_LEVEL = LOG_LEVEL
         self.dict = {'DEBUG': 2, 'INFO': 1, 'ERROR': 0}     
         
-        self.PWorkingDirectory = os.getcwd()
+        self.PWorkingDirectory = os.path.abspath(os.path.join(os.path.dirname( __file__ ), os.pardir))
              
     def Debug(self,msg):
-        targetfile=open(self.PWorkingDirectory+'Log\Log.log','a')
+        targetfile=open(self.PWorkingDirectory+'\APITestsLog\DebugLog.log','a')
         if(self.dict(self.LOG_LEVEL.upper()>=2)):
             print(time.strftime("%d/%m/%Y %I:%M:%S %p")+"DEBUG"+str(msg))
             targetfile.write(time.strftime("%d/%m/%Y %I:%M:%S %p")+"DEBUG:"+str(msg))
             targetfile.write("\n")
             targetfile.close()
     def Info(self, msg):
-        target = open(self.PWorkingDirectory + 'Log\log1.log', 'a')
+        target = open(self.PWorkingDirectory + '\APITestsLog\Infolog.log','a')
         if self.dict[self.LOG_LEVEL.upper()] >= 1:
             print (time.strftime("%d/%m/%Y %I:%M:%S %p") + " INFO: " + (str(msg)))
             target.write(time.strftime("%d/%m/%Y %I:%M:%S %p") + " Info: " + (str(msg)))
@@ -32,7 +32,7 @@ class Logger():
  
  
     def Error(self, msg):
-        target = open(self.PWorkingDirectory + 'Log\log1.log', 'a')
+        target = open(self.PWorkingDirectory + '\APITestsLog\Errorlog.log', 'a')
         if self.dict[self.LOG_LEVEL.upper()] > 0:
             print (time.strftime("%d/%m/%Y %I:%M:%S %p") + " ERROR: " + (str(msg)))
             target.write(time.strftime("%d/%m/%Y %I:%M:%S %p") + " Error: " + (str(msg)))
